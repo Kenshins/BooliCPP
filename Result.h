7437e8442f6c8cc3f357b0fdb5cd9bdbb5c73dc4
Martin Kleberger <martin.aviator@gmail.com>
2016-11-26 21:09:43 +0100

    First version of result

--- Result.h
+++ Result.h
@@ -6,8 +6,36 @@ class Result
 {
  public:
   Result();
-
+  GetTotalCount();
+  GetCount();
+  
  private:
+  int totalCount;
+  int count;
+};
+
+class AreaResult: public Result
+{
+
+};
+
+class ListingsResult: public Result
+{
+
+};
+
+class SoldResult: public Result
+{
+
+};
+
+class Listings
+{
+
+};
+
+class Location
+{
 
 };
 
