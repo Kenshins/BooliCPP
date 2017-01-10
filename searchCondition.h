90dc6d504c202a3569946215db56be1c1324133b
Martin Kleberger <martin.aviator@gmail.com>
2017-01-10 22:32:22 +0100

    Cont. work with searchCondition definition

--- searchCondition.h
+++ searchCondition.h
@@ -2,6 +2,23 @@
 #define SEARCHCONDITION_H
 #include <string>
 
+// Supporting datatypes
+
+class center
+{
+ public:
+  center();
+  center(int la, int ln);
+  void SetLatLong(int la, int ln);
+  void SetLat(int la);
+  void SetLong(int ln);
+
+ private:
+  int latitude;
+  int longitude;
+
+};
+
 class searchCondition_t
 {
  public:
@@ -17,9 +34,55 @@ class listingsSearchCondition_t : public searchCondition_t
  public:
   listingsSearchCondition_t();
   std::string SearchConditionResult();
-
+  void SetC(int la, int ln);
+  void SetDim(std::string d);
+  void SetBbox(std::string b);
+  void SetAreaId(std::string a);
+  void SetMinListPrice(double minLP);
+  void SetMaxListPrice(double maxLP);
+  void SetMinListSqmPrice(double minLSP);
+  void SetMaxListSqmPrice(double maxLSP);
+  void SetMinRooms(int minR);
+  void SetMaxRooms(int maxR);
+  void SetMaxRent(double maxRent);
+  void SetMinLivingArea(int minLA);
+  void SetMaxLivingArea(int maxLA);
+  void SetMinPlotArea(int minPA);
+  void SetMaxPlotArea(int maxPA);
+  void SetMinConstructionYear(int minCY);
+  void SetMaxConstructionYear(int maxCY);
+  void SetPriceDecrease(bool pD);
+  void SetIsNewConstruction(bool nC);
+  void SetIncludeUnset(bool iU);
+  void SetLimit(int l);
+  void SetOffset(int o);
+  
  private:
-    
+  center c; // Should be own type
+  std::string dim; // Should be own type
+  std::string bbox; // Should be own type
+  std::string areaId; // Should be own type
+  double minListPrice;
+  double maxListPrice;
+  double minListSqmPrice;
+  double maxListSqmPrice;
+  int minRooms;
+  int maxRooms;
+  double maxRent;
+  int minLivingArea;
+  int maxLivingArea;
+  int minPlotArea;
+  int maxPlotArea;
+  // objectType, should be a enum
+  int minConstructionYear;
+  int maxConstructionYear;
+  // minPublished date type
+  // maxPublished date type
+  bool priceDecrease;
+  bool isNewConstruction;
+  bool includeUnset;
+  int limit;
+  int offset;
 };
 
 class soldSearchCondition_t : public searchCondition_t
@@ -37,5 +100,6 @@ class areasSearchCondition_t : public searchCondition_t
   std::string SearchConditionResult();
 
 };
+
 
 #endif // SEARCHCONDITION_H
